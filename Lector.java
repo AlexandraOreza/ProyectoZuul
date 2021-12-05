1833e6f2b1914ec9c3428d039121e4dde385b8e6
Pablo Rosas Marín <69369694+PabloR9080@users.noreply.github.com>
2021-12-04 21:44:17 -0600

    pruebas de lectura

--- Lector.java
+++ Lector.java
@@ -1,3 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class Lector {
-    
+    private File archivo = null;
+
+    public void leeArchivo(String path){
+        try{
+            this.archivo = new File(path);
+            Scanner scanArchivo = new Scanner(archivo);
+            while(scanArchivo.hasNextLine()){
+                String linea = scanArchivo.nextLine();
+                String[] datos;
+                datos=linea.split(",");
+                System.out.println(datos[0]);
+            }
+            scanArchivo.close();
+        }catch(NullPointerException  | FileNotFoundException e){
+            System.err.println("Ha ocurrido un error con el archivo indicado.");
+        }
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Lector lector = new Lector();
+        lector.leeArchivo("files/room.txt");
+    }
 }
