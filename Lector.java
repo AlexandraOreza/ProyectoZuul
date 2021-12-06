27e394aedf84459ffde800dcfc8b5ab2bc4922ae
Pablo Rosas Marín <69369694+PabloR9080@users.noreply.github.com>
2021-12-06 04:29:04 -0600

    Metodos necesarios para generar cuartos agregados

--- Lector.java
+++ Lector.java
@@ -1,24 +1,88 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Lector {
     private File archivo = null;
 
-    public void leeArchivo(String path){
+    private String leeArchivo(String path){
+        String contenido ="";
         try{
             this.archivo = new File(path);
             Scanner scanArchivo = new Scanner(archivo);
             while(scanArchivo.hasNextLine()){
                 String linea = scanArchivo.nextLine();
-                String[] datos;
-                datos=linea.split(",");
-                System.out.println(datos[0]);
+                contenido += linea+"\n";
             }
             scanArchivo.close();
+            
         }catch(NullPointerException  | FileNotFoundException e){
             System.err.println("Ha ocurrido un error con el archivo indicado.");
         }
+        return contenido;
     }
+
+    public Collection<Room> obtenerCuartos(){
+        String contenido = leeArchivo("files/room.txt");
+        Collection<Room> Cuartos = new ArrayList<>();
+        Scanner scanContenido = new Scanner(contenido);
+        while(scanContenido.hasNextLine()){
+            String linea = scanContenido.nextLine();
+            List<String> datos = Arrays.asList(linea.split(","));
+            String nombre = datos.get(0).trim(); 
+            String descripcion = datos.get(1).trim();
+            Room cuarto = new Room(nombre, descripcion);
+            Cuartos.add(cuarto);
+        }
+        agregarSalidas(Cuartos);
+        scanContenido.close();
+        return Cuartos;
+    }
+
+    private void agregarSalidas(Collection<Room> Cuartos){
+        String infoCuartos = leeArchivo("files/exits.txt");
+        Scanner scanInfo = new Scanner(infoCuartos);
+        List<Room> cuartosViejos = Arrays.asList((Room[])Cuartos.toArray());
+        while(scanInfo.hasNextLine())
+        {
+            String salidas = scanInfo.nextLine();
+            List<String> datos = Arrays.asList(salidas.split(","));
+            for (Room cuartoAsignar : cuartosViejos) {
+                generaDirecciones(datos, cuartosViejos, cuartoAsignar);
+            }
+        }
+        scanInfo.close();
+    }
+
+    private void generaDirecciones(List<String> datosSalidas, Collection<Room> Cuartos, Room cuartoAsignar){
+        Room oeste, este, norte, sur = null;
+        oeste=este=norte=sur;
+        String cuartoOeste =  datosSalidas.get(1);
+        String cuartoEste =  datosSalidas.get(2);
+        String cuartoNorte =  datosSalidas.get(3);
+        String cuartoSur =  datosSalidas.get(4);
+        for (Room cuarto : Cuartos) {
+            String nombreCuarto = cuarto.getName();
+            if(cuartoOeste.equals(nombreCuarto)){
+                oeste = cuarto;
+            }
+            if(cuartoEste.equals(nombreCuarto)){
+                este = cuarto;
+            }
+            if(cuartoNorte.equals(nombreCuarto)){
+                norte = cuarto;
+            }
+            if(cuartoSur.equals(nombreCuarto)){
+                sur = cuarto;
+            }
+        }
+        cuartoAsignar.setExits(oeste, este, norte, sur);
+    }
+    
 }
