f1281cbdf403a039d19dec23f8160617d1143e01
Pablo Rosas Marín <69369694+PabloR9080@users.noreply.github.com>
2021-12-06 22:26:21 -0600

    Clase principal y constructor creados

---
+++ Command.java
@@ -0,0 +1,11 @@
+public class Command {
+    private String comando;
+    private String adicionComando;
+
+    public Command(String prefijo, String adicion){
+        comando = prefijo;
+        adicionComando = adicion;
+    }
+    
+
+}
